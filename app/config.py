1f94c57b4db6660528f1406ea00d22fdfb806477
Sherilogundeo <sherileidah@gmail.com>
2026-01-11 23:50:21 +0300

    update db url

--- app/config.py
+++ app/config.py
@@ -1,16 +1,25 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+database_url = os.getenv('POSTGRES_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3')
     BASE_URL = os.getenv('DEV_BASE_URL')
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = database_url
     BASE_URL = os.getenv('BASE_URL')
 
 def get_env(var_name):
